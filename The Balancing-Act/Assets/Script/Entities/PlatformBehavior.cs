8a87ed8b31ff114091fbdd48c54397182ce1c93c
L0raxeo <67399230+L0raxeo@users.noreply.github.com>
2021-04-11 19:50:42 -0400

    Testing Physics 2D Mechanics
    - Testing low pass accelerometer
    - added bouncy and slippery/low-friction physics 2D material

--- The Balancing-Act/Assets/Script/Entities/PlatformBehavior.cs
+++ The Balancing-Act/Assets/Script/Entities/PlatformBehavior.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlatformBehavior : MonoBehaviour
 {
+
+    protected Queue<Vector3> filterDataQueue = new Queue<Vector3>();
+    public int filterLength = 3; //you could change it in inspector
 
     private float torque = 45f;
 
@@ -11,9 +15,24 @@ public class PlatformBehavior : MonoBehaviour
         RotatePlatform();
     }
 
+    public Vector3 LowPassAccelerometer()
+    {
+        if (filterLength <= 0)
+            return Input.acceleration;
+        filterDataQueue.Enqueue(Input.acceleration);
+        filterDataQueue.Dequeue();
+
+        Vector3 vFiltered = Vector3.zero;
+        foreach (Vector3 v in filterDataQueue)
+            vFiltered += v;
+        vFiltered /= filterLength;
+        return vFiltered;
+    }
+
     private void RotatePlatform()
     {
-        float rotation = -Input.acceleration.x;
+        float rotation = -LowPassAccelerometer().x;
+        Debug.Log(LowPassAccelerometer().x);
         transform.rotation = Quaternion.Euler(0f, 0f, rotation * torque);
     }
 }
