2e20c7fd056d87da33c255ae215e6ed0ed6b553c
L0raxeo <67399230+L0raxeo@users.noreply.github.com>
2021-04-11 20:36:05 -0400

    Reverted

--- The Balancing-Act/Assets/Script/Entities/PlatformBehavior.cs
+++ The Balancing-Act/Assets/Script/Entities/PlatformBehavior.cs
@@ -1,12 +1,8 @@
 using UnityEngine;
-using System.Collections.Generic;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlatformBehavior : MonoBehaviour
 {
-
-    protected Queue<Vector3> filterDataQueue = new Queue<Vector3>();
-    public int filterLength = 3; //you could change it in inspector
 
     private float torque = 45f;
 
@@ -15,25 +11,15 @@ public class PlatformBehavior : MonoBehaviour
         RotatePlatform();
     }
 
-    public Vector3 LowPassAccelerometer()
-    {
-        if (filterLength <= 0)
-            return Input.acceleration;
-        filterDataQueue.Enqueue(Input.acceleration);
-        filterDataQueue.Dequeue();
-
-        Vector3 vFiltered = Vector3.zero;
-        foreach (Vector3 v in filterDataQueue)
-            vFiltered += v;
-        vFiltered /= filterLength;
-        return vFiltered;
-    }
-
     private void RotatePlatform()
     {
+<<<<<<< HEAD
         /*
         float rotation = -LowPassAccelerometer().x;
         Debug.Log(LowPassAccelerometer().x);
+=======
+        float rotation = -Input.acceleration.x;
+>>>>>>> parent of 8a87ed8 (Testing Physics 2D Mechanics)
         transform.rotation = Quaternion.Euler(0f, 0f, rotation * torque);
         */
 
