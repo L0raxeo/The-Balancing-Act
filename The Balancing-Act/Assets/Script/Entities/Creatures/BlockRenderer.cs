f79a60a9cc5df105682e736ad37e3c1dbd2afb3f
L0raxeo <67399230+L0raxeo@users.noreply.github.com>
2021-04-14 18:07:41 -0400

    Move forward
    - Completed block renderer script
    - added pause button, canvas, and non-functional score

--- The Balancing-Act/Assets/Script/Entities/Creatures/BlockRenderer.cs
+++ The Balancing-Act/Assets/Script/Entities/Creatures/BlockRenderer.cs
@@ -6,8 +6,15 @@ public class BlockRenderer : MonoBehaviour
     void Start()
     {
         setSprite();
+        setPosition();
         setSize();
         setTorque();
+    }
+
+    private void setPosition()
+    {
+        float xRan = Random.Range(-3, 3);
+        transform.position = new Vector3(xRan, 5f, 0f); // y will be changed to just above the camera
     }
 
     private void setSprite()
@@ -18,12 +25,15 @@ public class BlockRenderer : MonoBehaviour
 
     private void setSize()
     {
-
+        float size = Random.Range(0.75f, 2f);
+        transform.localScale = new Vector3(size, size, 1);
     }
 
     private void setTorque()
     {
-
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        float torque = Random.Range(-25, 25);
+        rb.AddTorque(torque);
     }
 
 }
