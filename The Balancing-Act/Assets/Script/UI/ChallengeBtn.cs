8596c2134ff9128f6fc2e7ae057e68c951c0389d
L0raxeo <67399230+L0raxeo@users.noreply.github.com>
2021-04-21 22:21:02 -0400

    Rotation Menu
    - Created menu for rotation mode
    - Added Challange btn functionality

---
+++ The Balancing-Act/Assets/Script/UI/ChallengeBtn.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeBtn : MonoBehaviour
+{
+
+    public void OnClick()
+    {
+        GameObject.FindObjectOfType<StateManager>().SetState("Game State", false);
+    }
+
+}
